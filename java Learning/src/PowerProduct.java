ff83cd0e453eed9c852efc218b829e42f143789c
reeta <reeta@LAPTOP-HJ7CNASL>
2020-10-30 21:03:55 +0530

    Pushing

---
+++ java Learning/src/PowerProduct.java
@@ -0,0 +1,14 @@
+
+public class PowerProduct {
+	public static void main(String[]args) {
+		int a = 1;
+		int b = 2;
+		int x = 4;
+		int formula=(a+b*x)*1;
+		for(int i = 2; i<10;i++) {
+			formula = formula *i;
+			
+		}
+		
+	}
+}
