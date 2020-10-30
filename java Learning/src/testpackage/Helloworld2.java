898e82048172f4ee48be5a37009d3f9dddb5770d
reeta <reeta@LAPTOP-HJ7CNASL>
2020-10-30 21:01:25 +0530

    My First Changes

---
+++ java Learning/src/testpackage/Helloworld2.java
@@ -0,0 +1,16 @@
+package testpackage;
+
+import java.util.Scanner;
+
+public class Helloworld2 {
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+    int a = scan.nextInt(42);
+    int b = scan.nextInt(100);
+    int c = scan.nextInt(125);
+    
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);}
+
+}
