60623d7c126fd8173053f990c35480b3bf0da8ed
Your Name <reeta.saini191@gmail.com>
2025-11-05 22:03:02 +0530

    my Commit

---
+++ JavaAndSpring/src/main/java/com/reeta/JavaAndSpring/JavaAndSpringApplication.java
@@ -0,0 +1,13 @@
+package com.reeta.JavaAndSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAndSpringApplication.class, args);
+	}
+
+}
